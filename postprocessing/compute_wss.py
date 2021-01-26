564c90640dad635d9b5382f15291509794f49392
Henrik Kjedsberg <henrik.kjeldsberg@live.no>
2021-01-27 00:55:18 +0100

    Refactor compute_wss with documentation and PEP-style Extracted dabla into separate function Note: is viscosity consistent between compute_wss and Artery.py?

--- postprocessing/compute_wss.py
+++ postprocessing/compute_wss.py
@@ -1,19 +1,28 @@
 from __future__ import print_function
-import os
-from time import time
-from hashlib import sha1
-from pathlib import Path
-import sys
 
-from dolfin import *
+from pathlib import Path
+from sys import argv
+
 import numpy as np
+from dolfin import *
 
 from stress import STRESS
 
 parameters["reorder_dofs_serial"] = False
 
 
-def main(case_path):
+def compute_hemodynamic_quantities(case_path):
+    """
+    Loads velocity fields from completed CFD simulation,
+    and computes and saves the following hemodynamic quantities:
+    (1) WSS - Wall shear stress
+    (2) TWSSG - Temporal wall shear stress gradient
+    (3) OSI - Oscillatory shear index
+    (4) RRT - Relative residence time
+
+    Args:
+        case_path (Path): Path to results from simulation
+    """
     # File paths
     file_path_x = case_path / "u0.xdmf"
     file_path_y = case_path / "u1.xdmf"
@@ -26,8 +35,8 @@ def main(case_path):
     f_2 = XDMFFile(MPI.comm_world, file_path_z.__str__())
 
     # Start post-processing from 2nd cycle using every 10th time step, or 2000 time steps per cycle
-    start = 2000   # save_data = 5 -> 10000 / 5 = 2000
-    step = 2       # save_data = 5 ->    10 / 5 = 2
+    start = 2000  # save_data = 5 -> 10000 / 5 = 2000
+    step = 2  # save_data = 5 ->    10 / 5 = 2
     dt = 0.951
 
     # Read mesh
@@ -42,12 +51,12 @@ def main(case_path):
     del f
 
     if MPI.rank(MPI.comm_world) == 0:
-        print("Define spaces")
-    uorder = 1
+        print("Define function spaces")
+    velocity_degree = 1
     V_b1 = VectorFunctionSpace(bm, "CG", 1)
     U_b1 = FunctionSpace(bm, "CG", 1)
-    V = VectorFunctionSpace(mesh, "CG", uorder)
-    U = FunctionSpace(mesh, "CG", uorder)
+    V = VectorFunctionSpace(mesh, "CG", velocity_degree)
+    U = FunctionSpace(mesh, "CG", velocity_degree)
 
     if MPI.rank(MPI.comm_world) == 0:
         print("Define functions")
@@ -74,26 +83,10 @@ def main(case_path):
     twssg = Function(V_b1)
     tau_prev = Function(V_b1)
 
+    # TODO: Is this viscosity consistent with Artery.py?
     mu = 0.0035
     stress = STRESS(u, 0.0, mu, mesh)
-
-    cpp_code = """
-    #include <pybind11/pybind11.h>
-    #include <dolfin.h>
-    namespace dolfin
-    {
-        void dabla(dolfin::GenericVector& a, dolfin::GenericVector& b) {
-            for (unsigned int i=0; i < b.size(); i++) {
-                b.setitem(i, pow((pow(a[i], 2) + pow(a[b.size() + i], 2) + pow(a[2 * b.size() + i], 2) ), 0.5));
-            }
-        }
-    }
-    PYBIND11_MODULE(SIGNATURE, m)
-    {
-        m.def("dabla", &dolfin::dabla);
-    }
-    """
-    func = compile_cpp_code(cpp_code).dabla
+    dabla = get_dabla_function()
 
     if MPI.rank(MPI.comm_world) == 0:
         print("Start 'simulation'")
@@ -101,14 +94,15 @@ def main(case_path):
     file_counter = start
     while True:
         if MPI.rank(MPI.comm_world) == 0:
-            print("Timestep", file_counter*5)
+            print("Timestep", file_counter * 5)
 
-        # Get u
+        # Read velocity components to respective functions and assign them to vector function u
         try:
             f_0.read_checkpoint(u0, "u0", file_counter)
             f_1.read_checkpoint(u1, "u1", file_counter)
             f_2.read_checkpoint(u2, "u2", file_counter)
         except:
+            print("Failed to read in velocity at file_counter={}".format(file_counter))
             break
         assign(u.sub(0), u0)
         assign(u.sub(1), u1)
@@ -123,7 +117,7 @@ def main(case_path):
 
         if MPI.rank(MPI.comm_world) == 0:
             print("Compute OSI")
-        func(tau.vector(), osi_.vector())
+        dabla(tau.vector(), osi_.vector())
         OSI.vector().axpy(1, osi_.vector())
 
         # Compute TWSSG
@@ -131,7 +125,7 @@ def main(case_path):
             print("Compute TWSSG")
         twssg.vector().set_local((tau.vector().get_local() - tau_prev.vector().get_local()) / dt)
         twssg.vector().apply("insert")
-        func(twssg.vector(), twssg_.vector())
+        dabla(twssg.vector(), twssg_.vector())
         TWSSG.vector().axpy(1, twssg_.vector())
 
         # Update tau
@@ -150,7 +144,7 @@ def main(case_path):
     WSS_new.vector()[:] = OSI.vector()[:]
 
     try:
-        func(WSS.vector(), rrt_.vector())
+        dabla(WSS.vector(), rrt_.vector())
         rrt_arr = rrt_.vector().get_local()
         rrt_arr[rrt_arr.nonzero()[0]] = 1e-6
         rrt_arr[np.isnan(rrt_arr)] = 1e-6
@@ -159,7 +153,7 @@ def main(case_path):
         OSI_arr = OSI.vector().get_local()
         OSI_arr[OSI_arr.nonzero()[0]] = 1e-6
         OSI_arr[np.isnan(OSI_arr)] = 1e-6
-        OSI.vector().set_local(0.5*(1 - rrt_arr / OSI_arr))
+        OSI.vector().set_local(0.5 * (1 - rrt_arr / OSI_arr))
         OSI.vector().apply("insert")
         save = True
     except:
@@ -191,7 +185,7 @@ def main(case_path):
     twssg_file << TWSSG
     del twssg_file
 
-    wss_file = File((case_path /  "WSS.xml.gz").__str__())
+    wss_file = File((case_path / "WSS.xml.gz").__str__())
     wss_file << WSS_new
     del wss_file
 
@@ -199,9 +193,41 @@ def main(case_path):
     wss_file << WSS_new
     del wss_file
 
-if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print("Run program as 'python path/to/compute_wss.py path/to/results/run_number/VTK'")
-        sys.exit(0)
 
-    main(Path.cwd() / sys.argv[1])
+def get_dabla_function():
+    """
+    Compiles a string in C++ and expose as a Python object (dabla),
+    used the compute several hemodynamic quantities.
+
+    Returns:
+        dabla: C++ compiled function
+    """
+
+    cpp_code = """
+    #include <pybind11/pybind11.h>
+    #include <dolfin.h>
+    namespace dolfin
+    {
+        void dabla(dolfin::GenericVector& a, dolfin::GenericVector& b) {
+            for (unsigned int i=0; i < b.size(); i++) {
+                b.setitem(i, pow((pow(a[i], 2) + pow(a[b.size() + i], 2) + pow(a[2 * b.size() + i], 2) ), 0.5));
+            }
+        }
+    }
+    PYBIND11_MODULE(SIGNATURE, m)
+    {
+        m.def("dabla", &dolfin::dabla);
+    }
+    """
+
+    dabla = compile_cpp_code(cpp_code).dabla
+
+    return dabla
+
+
+if __name__ == '__main__':
+    if len(argv) == 1:
+        print("Run program as 'python path/to/compute_wss.py path/to/results/run_number/VTK'")
+        exit(0)
+
+    compute_hemodynamic_quantities(Path.cwd() / argv[1])
