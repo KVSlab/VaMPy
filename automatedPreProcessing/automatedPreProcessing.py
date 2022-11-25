821e6916b45ac483c7f3c1e139f53e3a21906beb
Jørgen S. Dokken <dokken@simula.no>
2022-11-25 16:24:42 +0100

    Add mutually exclusive groups and more verbose formatter class

--- automatedPreProcessing/automatedPreProcessing.py
+++ automatedPreProcessing/automatedPreProcessing.py
@@ -346,14 +346,15 @@ def read_command_line():
     """
     Read arguments from commandline and return all values in a dictionary.
     """
-    parser = argparse.ArgumentParser(
-        description="Automated pre-processing for vascular modeling.")
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     description="Automated pre-processing for vascular modeling.")
 
-    parser.add_argument('-v', '--verbosity',
-                        dest='verbosity',
-                        type=str2bool,
-                        default=False,
-                        help="Activates the verbose mode.")
+    v = parser.add_mutually_exclusive_group(required=False)
+    v.add_argument('-v', '--verbosity',
+                   dest='verbosity',
+                   action='store_true',
+                   default=False,
+                   help="Activates the verbose mode.")
 
     parser.add_argument('-i', '--inputModel',
                         type=str,
@@ -361,13 +362,12 @@ def read_command_line():
                         dest='fileNameModel',
                         default='example/surface.vtp',
                         help="Input file containing the 3D model.")
-
-    parser.add_argument('-cM', '--compress-mesh',
-                        type=str2bool,
-                        required=False,
-                        dest='compressMesh',
-                        default=True,
-                        help="Compress output mesh after generation.")
+    cm = parser.add_mutually_exclusive_group(required=False)
+    cm.add_argument('-ncM', '--no-compress-mesh',
+                    default=True,
+                    dest='compressMesh',
+                    action='store_false',
+                    help="Do not compress output mesh after generation.")
 
     parser.add_argument('-sM', '--smoothingMethod',
                         type=str,
@@ -406,12 +406,13 @@ def read_command_line():
                         type=float,
                         help="Characteristic edge length used for meshing.")
 
-    parser.add_argument('-r', '--refine-region',
-                        dest="refineRegion",
-                        type=str2bool,
-                        default=False,
-                        help="Determine weather or not to refine a specific region of " +
-                             "the input model. Default is False.")
+    refine_region = parser.add_mutually_exclusive_group(required=False)
+    refine_region.add_argument('-r', '--refine-region',
+                               action='store_true',
+                               dest="refineRegion",
+                               default=False,
+                               help="Determine whether or not to refine a specific region of " +
+                                    "the input model")
 
     parser.add_argument('-rp', '--region-points',
                         dest="regionPoints",
@@ -423,17 +424,19 @@ def read_command_line():
                              "Example providing the points (0.1, 5.0, -1) and (1, -5.2, 3.21):" +
                              " --region-points 0.1 5 -1 1 5.24 3.21")
 
-    parser.add_argument('-at', '--atrium',
+    atrium = parser.add_mutually_exclusive_group(required=False)
+    atrium.add_argument('-at', '--atrium',
                         dest="isAtrium",
-                        type=str2bool,
+                        action="store_true",
                         default=False,
-                        help="Determine weather or not the model is an Atrium model. Default is False.")
+                        help="Determine whether or not the model is an Atrium model.")
 
-    parser.add_argument('-f', '--flowext',
-                        dest="flowExtension",
-                        default=True,
-                        type=str2bool,
-                        help="Add flow extensions to to the model.")
+    flowext = parser.add_mutually_exclusive_group(required=False)
+    flowext.add_argument('-nf', '--no-flowext',
+                         dest="flowExtension",
+                         default=True,
+                         action="store_false",
+                         help="Remove flow extensions from the model.")
 
     parser.add_argument('-fli', '--inletFlowext',
                         dest="inletFlowExtLen",
@@ -447,11 +450,12 @@ def read_command_line():
                         type=float,
                         help="Length of flow extensions at outlet(s).")
 
-    parser.add_argument('-vz', '--visualize',
+    visualize = parser.add_mutually_exclusive_group(required=False)
+    visualize.add_argument('-nvz', '--no-visualize',
                         dest="viz",
                         default=True,
-                        type=str2bool,
-                        help="Visualize surface, inlet, outlet and probes after meshing.")
+                        action="store_false",
+                        help="Skip visualizing surface, inlet, outlet and probes after meshing.")
 
     parser.add_argument('-sc', '--simulation-config',
                         type=str,
