cb489ea4e03041453e57847b72e9730d12f8b1ce
Kei <withwhom0215@gmail.com>
2022-11-27 00:21:08 -0500

    created automated meshing script for FSI aneurysm problem

--- automatedPreProcessing/common.py
+++ automatedPreProcessing/common.py
@@ -579,6 +579,57 @@ def generate_mesh(surface):
 
     return mesh, remeshSurface
 
+def generate_mesh_fsi(surface):
+    """
+    Generates a mesh suitable for FSI from a input surface model.
+
+    Args:
+        surface (vtkPolyData): Surface model to be meshed.
+
+    Returns:
+        mesh (vtkUnstructuredGrid): Output mesh
+        remeshedsurface (vtkPolyData): Remeshed version of the input model
+    """
+
+    from vmtkmeshgeneratorfsi import vmtkMeshGeneratorFsi
+
+    # Parameters
+    #TODO: It should be possible to change parameters from commmad line
+    #FIXEM: low TargetEdgeLength results in an error
+    TargetEdgeLength = 1.0
+    Solid_thickness = 0.3
+    meshGenerator = vmtkMeshGeneratorFsi()
+    meshGenerator.Surface = surface
+    meshGenerator.ElementSizeMode = 'edgelength'
+    meshGenerator.TargetEdgeLength = TargetEdgeLength
+    meshGenerator.MaxEdgeLength = 20*meshGenerator.TargetEdgeLength
+    meshGenerator.MinEdgeLength = 0.4*meshGenerator.TargetEdgeLength
+    meshGenerator.BoundaryLayer = 1
+    meshGenerator.NumberOfSubLayers = 2
+    meshGenerator.BoundaryLayerOnCaps = 0
+    meshGenerator.BoundaryLayerThicknessFactor = Solid_thickness / TargetEdgeLength 
+    meshGenerator.SubLayerRatio = 1
+    meshGenerator.Tetrahedralize = 1
+    meshGenerator.VolumeElementScaleFactor = 0.8
+    meshGenerator.EndcapsEdgeLengthFactor = 1.0
+
+    # Cells and walls numbering
+    meshGenerator.SolidSideWallId = 11
+    meshGenerator.InterfaceId_fsi = 22
+    meshGenerator.InterfaceId_outer = 33
+    meshGenerator.VolumeId_fluid = 0  # (keep to 0)
+    meshGenerator.VolumeId_solid = 1
+
+    meshGenerator.Execute()
+
+    # Remeshed surface, store for later
+    remeshSurface = meshGenerator.RemeshedSurface
+
+    # Full mesh
+    mesh = meshGenerator.Mesh
+
+    return mesh, remeshSurface
+
 
 def find_boundaries(model_path, mean_inflow_rate, network, mesh, verbose_print, is_atrium):
     """
