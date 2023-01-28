a1c28fb1ea4a2f61ddf479aec8218efdbdcf0dda
Henrik Kjedsberg <henrik.kjeldsberg@live.no>
2023-01-28 01:36:00 +0100

    Update simulate file

--- src/vampy/automatedPreProcessing/simulate.py
+++ src/vampy/automatedPreProcessing/simulate.py
@@ -1,6 +1,6 @@
 import errno
 import json
-from os import path, walk
+from os import path
 
 import paramiko
 
@@ -37,18 +37,14 @@ def run_simulation(config_path, local_dir, case_name):
         hostname = config['hostname']
         username = config['username']
         password = config['password']
-        remote_folder = config['remoteDataDir']
-        local_folder = config['localDataDir']
-        simulation_folder = config['simDataDir']
-        post_processing_folder = config['postProcDataDir']
+        remote_vampy_folder = config['remote_vampy_folder']
+        local_mesh_folder = config['local_mesh_folder']
         job_script = config['job_script']
-        flow_rate = config['flow_rate']
-        problem_file = config['problem_file']
     except KeyError:
         raise ValueError('Invalid configuration file')
-
+    
     # Use case folder if local folder is blank
-    local_dir = local_dir if local_folder == '' else local_folder
+    local_dir = local_dir if local_mesh_folder == '' else local_mesh_folder
 
     # Get path to home folder on remote
     client.connect(hostname, username=username, password=password)
@@ -56,47 +52,17 @@ def run_simulation(config_path, local_dir, case_name):
     home = str(stdout.read().strip().decode("utf-8"))
 
     sftp = client.open_sftp()
-    remote_folder = path.join(home, remote_folder)
+    remote_vampy_folder = path.join(home, remote_vampy_folder)
 
-    # Upload run script
-    sftp.chdir(remote_folder)
+    # Upload run script from mesh folder
+    sftp.chdir(remote_vampy_folder)
     sftp.put(path.join(local_dir, job_script), job_script)
 
     # Upload simulation scripts
     sftp.chdir(home)
-    for dirpath, _, filenames in walk(path.join(simulation_folder, "probe")):
-        try:
-            sftp.listdir(path.join(home, "probe"))
-        except IOError:
-            sftp.mkdir(path.join(home, "probe"))
-
-        for filename in filenames:
-            sftp.put(path.join(dirpath, filename), path.join(home, "probe", filename))
-
-    if not exists(sftp, "probe"):
-        sftp.put(path.join(simulation_folder, "probe"), "probe")
-    if not exists(sftp, "Womersley.py"):
-        sftp.put(path.join(simulation_folder, "Womersley.py"), "Womersley.py")
-    if not exists(sftp, "Probe.py"):
-        sftp.put(path.join(simulation_folder, "Probe.py"), "Probe.py")
-    if not exists(sftp, flow_rate):
-        sftp.put(path.join(simulation_folder, flow_rate), flow_rate)
-    if not exists(sftp, problem_file):
-        sftp.put(path.join(simulation_folder, problem_file), problem_file)
-
-    # Upload post-processing scripts
-    if not exists(sftp, "compute_flow_and_simulation_metrics.py"):
-        sftp.put(path.join(post_processing_folder, "compute_flow_and_simulation_metrics.py"),
-                 "compute_flow_and_simulation_metrics.py")
-    if not exists(sftp, "compute_hemodynamic_indices.py"):
-        sftp.put(path.join(post_processing_folder, "compute_hemodynamic_indices.py"), "compute_hemodynamic_indices.py")
-    if not exists(sftp, "visualize_probes.py"):
-        sftp.put(path.join(post_processing_folder, "visualize_probes.py"), "visualize_probes.py")
-    if not exists(sftp, "postprocessing_common.py"):
-        sftp.put(path.join(post_processing_folder, "postprocessing_common.py"), "postprocessing_common.py")
 
     # Upload mesh, probe points and info
-    sftp.chdir(path.join(remote_folder, "oasis/mesh"))
+    sftp.chdir(path.join(remote_vampy_folder, "src/vampy/simulation"))
     if not exists(sftp, case_name + ".xml.gz"):
         sftp.put(path.join(local_dir, case_name + ".xml.gz"), case_name + ".xml.gz")
     if not exists(sftp, case_name + "_probe_point"):
@@ -104,13 +70,10 @@ def run_simulation(config_path, local_dir, case_name):
     if not exists(sftp, case_name + "_info.json"):
         sftp.put(path.join(local_dir, case_name + "_info.json"), case_name + "_info.json")
 
-    if not exists(sftp, path.join(remote_folder, "results_{}".format(case_name))):
-        sftp.mkdir(path.join(remote_folder, "results_{}".format(case_name)))
-
     sftp.close()
 
     # Run script
-    stdin, stdout, stderr = client.exec_command('sbatch {}'.format(path.join(remote_folder, job_script)))
+    stdin, stdout, stderr = client.exec_command('sbatch {}'.format(path.join(remote_vampy_folder, job_script)))
 
     for msg in stdout:
         print(msg)
