219d36c41496d52fca24332bd706f98d476a8200
Kei <withwhom0215@gmail.com>
2023-02-13 16:28:33 +0100

    modified values of parameters for fsi meshing. Specifc to case9 with approx. 170k

--- automatedPreProcessing/common.py
+++ automatedPreProcessing/common.py
@@ -596,14 +596,14 @@ def generate_mesh_fsi(surface):
     # Parameters
     #TODO: It should be possible to change parameters from commmad line
     #FIXEM: low TargetEdgeLength results in an error
-    TargetEdgeLength = 0.3
-    Solid_thickness = 0.2
+    TargetEdgeLength = 0.38
+    Solid_thickness = 0.25
     meshGenerator = vmtkMeshGeneratorFsi()
     meshGenerator.Surface = surface
     meshGenerator.ElementSizeMode = 'edgelength'
     meshGenerator.TargetEdgeLength = TargetEdgeLength
     meshGenerator.MaxEdgeLength = 15*meshGenerator.TargetEdgeLength
-    meshGenerator.MinEdgeLength = 0.4*meshGenerator.TargetEdgeLength
+    meshGenerator.MinEdgeLength = 5*meshGenerator.TargetEdgeLength
     meshGenerator.BoundaryLayer = 1
     meshGenerator.NumberOfSubLayers = 2
     meshGenerator.BoundaryLayerOnCaps = 0
