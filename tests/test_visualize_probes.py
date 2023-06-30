f5a8c9584b97df457df8ac4cd307f85cb38e3ba4
Henrik Kjedsberg <henrik.kjeldsberg@live.no>
2023-06-30 14:32:03 +0200

    Update test

--- tests/test_visualize_probes.py
+++ tests/test_visualize_probes.py
@@ -3,22 +3,50 @@ from os import path, remove
 from vampy.automatedPostprocessing.visualize_probes import visualize_probes
 
 
-def test_visualize_probes():
+def test_visualize_all_probes():
     # Path to test results and params
+    T = 100
+    dt = 0.1
     probe_path = "tests/test_data/results/Probes"
     probe_frequency = 100
-    file_path = path.join(probe_path, "Probes.png")
+    u_p_path = path.join(probe_path, "velocity_and_pressure.png")
+    ke_path = path.join(probe_path, "kinetic_energy.png")
+    spectrum_path = path.join(probe_path, "energy_spectrum.png")
+    spectrogram_path = path.join(probe_path, "spectrogram.png")
 
     # Run post-processing
-    visualize_probes(probe_path, probe_frequency, save_figure=True, show_figure=False)
+    visualize_probes(probe_path, probe_frequency, T, dt, probes_to_plot=[], save_figure=True, show_figure=False)
 
     # Check that output image exists
-    assert path.exists(file_path)
-    assert path.getsize(file_path) > 0
+    for file_path in [u_p_path, ke_path, spectrum_path, spectrogram_path]:
+        assert path.exists(file_path)
+        assert path.getsize(file_path) > 0
 
-    # Remove generatedoutput
-    remove(file_path)
+        # Remove generatedoutput
+        remove(file_path)
+
+
+def test_visualize_two_probes():
+    # Path to test results and params
+    probes = [1, 2]
+    T = 100
+    dt = 0.1
+    probe_path = "tests/test_data/results/Probes"
+    probe_frequency = 100
+
+    # Run post-processing
+    visualize_probes(probe_path, probe_frequency, T, dt, probes_to_plot=probes, save_figure=True, show_figure=False)
+
+    # Check that output image exists
+    for probe in probes:
+        results_path = path.join(probe_path, f"probes_{probe}.png")
+        assert path.exists(results_path)
+        assert path.getsize(results_path) > 0
+
+        # Remove generatedoutput
+        remove(results_path)
 
 
 if __name__ == "__main__":
-    test_visualize_probes()
+    test_visualize_all_probes()
+    test_visualize_two_probes()
