e094ea5d1221fb38513d079472ee2f6636d85a35
Kei <withwhom0215@gmail.com>
2023-08-16 22:42:18 +0200

    modify dependency

--- src/vampy/__init__.py
+++ src/vampy/__init__.py
@@ -8,21 +8,27 @@ from .automatedPostprocessing import compute_velocity_and_pressure
 from .automatedPostprocessing import postprocessing_common
 from .automatedPostprocessing import visualize_probes
 # Imports from pre-processing
-from .automatedPreprocessing import DisplayData
-from .automatedPreprocessing import ImportData
-from .automatedPreprocessing import NetworkBoundaryConditions
-from .automatedPreprocessing import ToolRepairSTL
-from .automatedPreprocessing import automated_preprocessing
-from .automatedPreprocessing import preprocessing_common
-from .automatedPreprocessing import simulate
-from .automatedPreprocessing import visualize
-from .automatedPreprocessing import vmtk_pointselector
+try :
+    from .automatedPreprocessing import DisplayData
+    from .automatedPreprocessing import ImportData
+    from .automatedPreprocessing import NetworkBoundaryConditions
+    from .automatedPreprocessing import ToolRepairSTL
+    from .automatedPreprocessing import automated_preprocessing
+    from .automatedPreprocessing import preprocessing_common
+    from .automatedPreprocessing import simulate
+    from .automatedPreprocessing import visualize
+    from .automatedPreprocessing import vmtk_pointselector
+except ModuleNotFoundError :
+    print("morphman is not installed, automated preprocessing is not available")
 # Imports from simulation scripts
-from .simulation import Artery
-from .simulation import Atrium
-from .simulation import Probe
-from .simulation import Womersley
-from .simulation import simulation_common
+try :
+    from .simulation import Artery
+    from .simulation import Atrium
+    from .simulation import Probe
+    from .simulation import Womersley
+    from .simulation import simulation_common
+except ModuleNotFoundError :
+    print("oasis is not installed, running CFD is not available")
 
 
 meta = metadata("vampy")
--- src/vampy/simulation/__init__.py
+++ src/vampy/simulation/__init__.py
@@ -1,5 +1,8 @@
-from . import Artery
-from . import Atrium
-from . import simulation_common
-from . import Womersley
-from . import Probe
+try :
+    from . import Artery
+    from . import Atrium
+    from . import simulation_common
+    from . import Womersley
+    from . import Probe
+except ModuleNotFoundError :
+    print("oasis is not installed, running CFD is not available")
