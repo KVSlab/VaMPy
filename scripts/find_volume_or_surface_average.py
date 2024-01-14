bbb7f5a8f01c8e31079055ba3dcac99a25b6154f
Henrik Kjedsberg <henrik.kjeldsberg@live.no>
2024-01-14 23:36:44 +0100

    Test timeing

--- scripts/find_volume_or_surface_average.py
+++ scripts/find_volume_or_surface_average.py
@@ -115,7 +115,7 @@ if __name__ == '__main__':
     conditions = ["sr"]
     cases = ['1029']
     cycle = 3
-    is_local =False
+    is_local = False
     metrics = ['kinetic_energy', 'turbulent_kinetic_energy', 'blood_residence_time', 'hemodynamics']
     for metric in metrics:
         for case in cases:
