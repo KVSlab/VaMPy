545a0084e136cd1be3a74fcc261e4936b179e233
Henrik Kjedsberg <henrik.kjeldsberg@live.no>
2024-01-19 20:59:50 +0100

    Lowercase

--- scripts/run_clip.sh
+++ scripts/run_clip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CASES=(0003)
-CONDITION=SR
+CONDITION=sr
 for CASE in "${CASES[@]}"; do
   echo "Conevrting case ${CASE} , condiiton ${CONDITION}"
   python landmark_left_atrium.py --case $CASE --condition $CONDITION
